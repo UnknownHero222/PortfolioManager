abfda37f393d8b05a5be8f5d9052d3c05df3d41f
UnknwonHero222 <yusupmarat2@gmail.com>
2021-05-31 15:30:19 +0300

    Elements for portfolio title setup was added

--- portfolio_title_dlg.cpp
+++ portfolio_title_dlg.cpp
@@ -1,14 +1,16 @@
 #include "portfolio_title_dlg.h"
+
 #include "ui_portfolio_title_dlg.h"
 
-PortfolioTitleDlg::PortfolioTitleDlg(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::PortfolioTitleDlg)
-{
-    ui->setupUi(this);
+PortfolioTitleDlg::PortfolioTitleDlg(QWidget *parent)
+    : QDialog(parent), ui(new Ui::PortfolioTitleDlg) {
+  ui->setupUi(this);
 }
 
-PortfolioTitleDlg::~PortfolioTitleDlg()
-{
-    delete ui;
+PortfolioTitleDlg::~PortfolioTitleDlg() { delete ui; }
+
+void PortfolioTitleDlg::on_ok_btn_clicked() {
+  portfolio_title = ui->portfolio_title_pln->toPlainText();
 }
+
+void PortfolioTitleDlg::on_cancel_btn_clicked() {}
--- portfolio_title_dlg.h
+++ portfolio_title_dlg.h
@@ -7,16 +7,22 @@ namespace Ui {
 class PortfolioTitleDlg;
 }
 
-class PortfolioTitleDlg : public QDialog
-{
-    Q_OBJECT
+class PortfolioTitleDlg : public QDialog {
+  Q_OBJECT
 
-public:
-    explicit PortfolioTitleDlg(QWidget *parent = nullptr);
-    ~PortfolioTitleDlg();
+ public:
+  explicit PortfolioTitleDlg(QWidget *parent = nullptr);
+  ~PortfolioTitleDlg();
 
-private:
-    Ui::PortfolioTitleDlg *ui;
+  QString portfolio_title;
+
+ private slots:
+  void on_ok_btn_clicked();
+
+  void on_cancel_btn_clicked();
+
+ private:
+  Ui::PortfolioTitleDlg *ui;
 };
 
-#endif // PORTFOLIO_TITLE_DLG_H
+#endif  // PORTFOLIO_TITLE_DLG_H
